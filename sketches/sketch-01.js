35bb4ad5a419da738081f95b9cd0032b92e6feec
unknown <mkekae@gmail.com>
2022-09-27 19:54:11 +0200

    modified sketch-01.js

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,14 +1,20 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+	dimensions: [2048, 2048],
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-  };
+	return ({ context, width, height }) => {
+		context.fillStyle = "black";
+		context.fillRect(0, 0, width, height);
+
+		for (let i = 0; i < 5; i++) {
+			context.beginPath();
+			context.fillStyle = "red";
+			context.fillRect(100 + 80 * i, 100, 60, 60);
+		}
+	};
 };
 
 canvasSketch(sketch, settings);
